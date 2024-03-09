70c7165cd52f630436c4bd1e131bde068ce191e7
Jeofferson Dela Peña <jeoffersond@gmail.com>
2024-03-09 14:09:18 +0800

    feat(ios): create tasks tabs

--- task-manager-ios/task-manager-ios/Presentation/Screens/Main/ContentView.swift
+++ task-manager-ios/task-manager-ios/Presentation/Screens/Main/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            NavigationStack {
+                TasksListScreen()
+            }
+            .tabItem {
+                Label(
+                    "Tasks",
+                    systemImage: "checklist"
+                )
+            }
+            
+            NavigationStack {
+                CompletedTasksListScreen()
+            }
+            .tabItem {
+                Label(
+                    "Completed Tasks",
+                    systemImage: "checklist.checked"
+                )
+            }
         }
-        .padding()
     }
 }
 
---
+++ task-manager-ios/task-manager-ios/Presentation/Screens/Tasks List Screen/TasksListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  TasksListScreen.swift
+//  task-manager-ios
+//
+//  Created by Jeofferson Dela Peña on 3/9/24.
+//
+
+import SwiftUI
+
+struct TasksListScreen: View {
+    var body: some View {
+        Text("Tasks List Screen")
+    }
+}
+
+#Preview {
+    TasksListScreen()
+}
