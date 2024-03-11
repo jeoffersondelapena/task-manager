7224e97bd692482bc20b87a9e688c6d773d6854d
Jeofferson Dela Peña <jeoffersond@gmail.com>
2024-03-11 14:14:30 +0800

    refactor(android): clean up initial composables

--- task-manager-android/app/src/main/java/com/jeoffersondelapena/task_manager_android/presentation/screen/tasks_list_screen/TasksListScreen.kt
+++ task-manager-android/app/src/main/java/com/jeoffersondelapena/task_manager_android/presentation/screen/tasks_list_screen/TasksListScreen.kt
@@ -1,11 +1,28 @@
 package com.jeoffersondelapena.task_manager_android.presentation.screen.tasks_list_screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jeoffersondelapena.task_manager_android.presentation.res.ui.theme.TaskmanagerandroidTheme
 
 @Composable
-fun TasksListScreen(viewModel: TasksListViewModel = viewModel()) {
+fun TasksListScreen(modifier: Modifier = Modifier, viewModel: TasksListViewModel = viewModel()) {
     viewModel.getTasks()
-    Text("Hello, Hilt!")
+
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Hello, Hilt!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TasksListScreenPreview() {
+    TaskmanagerandroidTheme {
+        TasksListScreen(viewModel = TasksListViewModel.sample)
+    }
 }
--- task-manager-android/app/src/main/java/com/jeoffersondelapena/task_manager_android/presentation/screen/tasks_list_screen/TasksListViewModel.kt
+++ task-manager-android/app/src/main/java/com/jeoffersondelapena/task_manager_android/presentation/screen/tasks_list_screen/TasksListViewModel.kt
@@ -1,6 +1,9 @@
 package com.jeoffersondelapena.task_manager_android.presentation.screen.tasks_list_screen
 
 import androidx.lifecycle.ViewModel
+import com.jeoffersondelapena.task_manager_android.data.local.service.TaskLocalService
+import com.jeoffersondelapena.task_manager_android.data.remote.service.TaskRemoteService
+import com.jeoffersondelapena.task_manager_android.data.repository.TaskRepositoryImpl
 import com.jeoffersondelapena.task_manager_android.domain.repository.TaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,5 +14,14 @@ class TasksListViewModel @Inject constructor(
 ) : ViewModel() {
     fun getTasks() {
         repository.getTasks()
+    }
+
+    companion object {
+        val sample = TasksListViewModel(
+            TaskRepositoryImpl(
+                TaskRemoteService(),
+                TaskLocalService()
+            )
+        )
     }
 }
