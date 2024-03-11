e212f1aa3f2f5f88e80995fcf507b3eeb4d004bd
Jeofferson Dela Peña <jeoffersond@gmail.com>
2024-03-12 07:35:32 +0800

    feat(android): display completed tasks

--- task-manager-android/app/src/main/java/com/jeoffersondelapena/task_manager_android/presentation/screen/completed_tasks_list_screen/CompletedTasksListScreen.kt
+++ task-manager-android/app/src/main/java/com/jeoffersondelapena/task_manager_android/presentation/screen/completed_tasks_list_screen/CompletedTasksListScreen.kt
@@ -1,19 +1,38 @@
 package com.jeoffersondelapena.task_manager_android.presentation.screen.completed_tasks_list_screen
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jeoffersondelapena.task_manager_android.presentation.core.TasksListState
 import com.jeoffersondelapena.task_manager_android.presentation.core.TasksListViewModel
+import com.jeoffersondelapena.task_manager_android.presentation.reusable_view.TaskItem
+import com.jeoffersondelapena.task_manager_android.presentation.reusable_view.swipe_refresh.PullRefreshIndicator
+import com.jeoffersondelapena.task_manager_android.presentation.reusable_view.swipe_refresh.pullRefresh
+import com.jeoffersondelapena.task_manager_android.presentation.reusable_view.swipe_refresh.rememberPullRefreshState
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,8 +46,34 @@ fun CompletedTasksListScreen(viewModel: TasksListViewModel) {
             )
         },
         content = { padding ->
-            Box(modifier = Modifier.padding(padding).fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text("Completed Tasks List Screen")
+            val refreshScope = rememberCoroutineScope()
+            var refreshing by remember { mutableStateOf(false) }
+
+            fun refresh() = refreshScope.launch {
+                refreshing = true
+                viewModel.getTasks(isFromSwipeRefresh = true)
+                refreshing = false
+            }
+
+            val state = rememberPullRefreshState(refreshing, ::refresh)
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(padding)
+            ) {
+                if (viewModel.state.isLoading) {
+                    CircularProgressIndicator()
+                }
+
+                Box(Modifier.pullRefresh(state)) {
+                    LazyColumn(Modifier.fillMaxSize()) {
+                        items(viewModel.state.completedTasks) { task ->
+                            TaskItem(viewModel, task)
+                        }
+                    }
+
+                    PullRefreshIndicator(refreshing, state, Modifier.align(Alignment.TopCenter))
+                }
             }
         }
     )
